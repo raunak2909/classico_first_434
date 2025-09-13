f7418581a68ef15a38e87b0b2c7fccc6c2924927
raunakpurohit <raunak.wscubetech@gmail.com>
2025-09-13 20:56:25 +0530

    button and layout widgets

--- lib/test/home_page.dart
+++ lib/test/home_page.dart
@@ -5,52 +5,52 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      /*appBar: AppBar(
         leading: Icon(Icons.menu),
         title: Text("Home", style: TextStyle(fontFamily: "poppins")),
         backgroundColor: Color(0xff5C6BC0),
         foregroundColor: Colors.white,
-      ),
+      ),*/
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: TextField(
-          onChanged: (value){
-            print(value);
-          },
-          enabled: true,
-          //obscureText: true,
-          keyboardType: TextInputType.emailAddress,
-          //obscuringCharacter: "*",
-          decoration: InputDecoration(
-            //prefixText: "Mr/Mrs. ",
-            prefixIcon: Icon(Icons.email),
-            //suffixText: "@gmail.com",
-              suffixIcon: Icon(Icons.visibility_off),
-            labelText: "Email",
-            hintText: "Enter your email here..",
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(7),
-              borderSide: BorderSide(
-                color: Colors.blue,
-                width: 1
-              )
+        padding: const EdgeInsets.only(top: 80, left: 21, right: 21),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Icon(Icons.menu),
+                Icon(Icons.notifications)
+              ],
             ),
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(21),
-              borderSide: BorderSide(
-                color: Colors.green,
-                width: 2
-              )
+            SizedBox(
+              height:11,
             ),
-            disabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(7),
-                borderSide: BorderSide(
-                    color: Colors.grey,
-                    width: 1
+            TextField(
+              decoration: InputDecoration(
+                fillColor: Colors.grey.shade200,
+                filled: true,
+                hintText: "Search...",
+                prefixIcon: Icon(Icons.search),
+                suffixIcon: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                      width: 1,
+                      height: 20,
+                      color: Colors.black,
+                    ),
+                    SizedBox(width: 11,),
+                    Icon(Icons.filter_alt_outlined),
+                  ],
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(50),
+                  borderSide: BorderSide.none
                 )
+              ),
             )
-          ),
-        ),
+          ],
+        )
       )
       /*floatingActionButton: FloatingActionButton(
         backgroundColor: Color(0xff5C6BC0),
@@ -63,6 +63,260 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+
+///Container(
+//           width: 300,
+//           color: Colors.grey,
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.spaceAround,
+//             crossAxisAlignment: CrossAxisAlignment.stretch,
+//             children: [
+//               Container(
+//                 width: 200,
+//                 height: 50,
+//                 color: Colors.orange,
+//                 child: SingleChildScrollView(
+//                   scrollDirection: Axis.horizontal,
+//                   child: Row(
+//                     children: [
+//                       Container(
+//                         width: 50,
+//                         height: 50,
+//                         color: Colors.blue,
+//                       ),
+//                       Container(
+//                         width: 50,
+//                         height: 50,
+//                         color: Colors.red,
+//                       ),
+//                       Container(
+//                         width: 50,
+//                         height: 50,
+//                         color: Colors.amber,
+//                       ),
+//                       Container(
+//                         width: 50,
+//                         height: 50,
+//                         color: Colors.green,
+//                       ),
+//                       Container(
+//                         width: 50,
+//                         height: 50,
+//                         color: Colors.purple,
+//                       ),
+//                       Container(
+//                         width: 50,
+//                         height: 50,
+//                         color: Colors.pink,
+//                       ),
+//                       Container(
+//                         width: 50,
+//                         height: 50,
+//                         color: Colors.purple,
+//                       ),
+//                       Container(
+//                         width: 50,
+//                         height: 50,
+//                         color: Colors.pink,
+//                       ),
+//                     ],
+//                   ),
+//                 ),
+//               ),
+//               Container(
+//                 width: 50,
+//                 height: 50,
+//                 color: Colors.blue,
+//               ),
+//               Container(
+//                 width: 50,
+//                 height: 50,
+//                 color: Colors.red,
+//               ),
+//               Container(
+//                 width: 50,
+//                 height: 50,
+//                 color: Colors.amber,
+//               ),
+//               Container(
+//                 width: 50,
+//                 height: 50,
+//                 color: Colors.green,
+//               ),
+//               Container(
+//                 width: 50,
+//                 height: 50,
+//                 color: Colors.purple,
+//               ),
+//               Container(
+//                 width: 50,
+//                 height: 50,
+//                 color: Colors.pink,
+//               ),
+//               /*Container(
+//                 width: 50,
+//                 height: 50,
+//                 color: Colors.orange,
+//               ),
+//               Container(
+//                 width: 50,
+//                 height: 50,
+//                 color: Colors.blue,
+//               ),
+//               Container(
+//                 width: 50,
+//                 height: 50,
+//                 color: Colors.red,
+//               ),
+//               Container(
+//                 width: 50,
+//                 height: 50,
+//                 color: Colors.amber,
+//               ),
+//               Container(
+//                 width: 50,
+//                 height: 50,
+//                 color: Colors.orange,
+//               ),
+//               Container(
+//                 width: 50,
+//                 height: 50,
+//                 color: Colors.blue,
+//               ),
+//               Container(
+//                 width: 50,
+//                 height: 50,
+//                 color: Colors.red,
+//               ),
+//               Container(
+//                 width: 50,
+//                 height: 50,
+//                 color: Colors.amber,
+//               ),
+//               Container(
+//                 width: 50,
+//                 height: 50,
+//                 color: Colors.green,
+//               ),
+//               Container(
+//                 width: 50,
+//                 height: 50,
+//                 color: Colors.purple,
+//               ),
+//               Container(
+//                 width: 50,
+//                 height: 50,
+//                 color: Colors.pink,
+//               ),
+//               Container(
+//                 width: 50,
+//                 height: 50,
+//                 color: Colors.orange,
+//               ),
+//               Container(
+//                 width: 50,
+//                 height: 50,
+//                 color: Colors.blue,
+//               ),
+//               Container(
+//                 width: 50,
+//                 height: 50,
+//                 color: Colors.red,
+//               ),
+//               Container(
+//                 width: 50,
+//                 height: 50,
+//                 color: Colors.amber,
+//               ),*/
+//             ],
+//           ),
+//         )
+
+///ElevatedButton(
+//             style: ElevatedButton.styleFrom(
+//               minimumSize: Size(100, 50),
+//               maximumSize: Size(200, 50),
+//               backgroundColor: Colors.orange,
+//               foregroundColor: Colors.white,
+//               shape: RoundedRectangleBorder(
+//                 borderRadius: BorderRadius.circular(11)
+//               )
+//             ),
+//             onPressed: (){
+//           print("Tapped!!");
+//         }, child: Row(
+//           children: [
+//             Icon(Icons.add),
+//             SizedBox(width: 21,),
+//             Text('Click me')
+//           ],
+//         ))
+
+///InkWell(
+//           onTap: (){
+//             print("Tapped!!");
+//           },
+//           child: Container(
+//             width: 200,
+//             height: 50,
+//             decoration: BoxDecoration(
+//               borderRadius: BorderRadius.circular(25),
+//               //color: Colors.orange
+//               border: Border.all(
+//                 color: Color(0xff5C6BC0),
+//                 width: 2
+//               )
+//             ),
+//             child: Center(child: Text("Click me", style: TextStyle(fontSize: 16, color: Colors.black),)),
+//           ),
+//         )
+
+///IconButton(onPressed: (){
+//
+//         }, icon: Icon(Icons.add))
+
+///TextButton(onPressed: (){
+//           print("Tapped!!");
+//         }, child: Text("Click me"))
+
+///TextField(
+//           onChanged: (value){
+//             print(value);
+//           },
+//           enabled: true,
+//           //obscureText: true,
+//           keyboardType: TextInputType.emailAddress,
+//           //obscuringCharacter: "*",
+//           decoration: InputDecoration(
+//             //prefixText: "Mr/Mrs. ",
+//             prefixIcon: Icon(Icons.email),
+//             //suffixText: "@gmail.com",
+//               suffixIcon: Icon(Icons.visibility_off),
+//             labelText: "Email",
+//             hintText: "Enter your email here..",
+//             enabledBorder: OutlineInputBorder(
+//               borderRadius: BorderRadius.circular(7),
+//               borderSide: BorderSide(
+//                 color: Colors.blue,
+//                 width: 1
+//               )
+//             ),
+//             focusedBorder: OutlineInputBorder(
+//               borderRadius: BorderRadius.circular(21),
+//               borderSide: BorderSide(
+//                 color: Colors.green,
+//                 width: 2
+//               )
+//             ),
+//             disabledBorder: OutlineInputBorder(
+//               borderRadius: BorderRadius.circular(7),
+//                 borderSide: BorderSide(
+//                     color: Colors.grey,
+//                     width: 1
+//                 )
+//             )
+//           ),
+//         ),
 
 ///Image
 ///Container(
